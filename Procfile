4cdeba421d2f89eff2d938b79de374b2542d5020
George Akor <georgejupiter303@gmail.com>
2022-11-01 00:31:38 +0100

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn alaric.wsgi --log-file -
---
+++ beric/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BericConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'beric'
